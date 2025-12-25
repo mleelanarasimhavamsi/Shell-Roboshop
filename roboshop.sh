633652876901e1623de1dcbba3008d520ee84c8b
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-25 05:37:53 +0530

    new repo

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 
 AMIID="ami-09c813fb71547fc4f"
 SG_ID="sg-0efb46bbc7c5d112f"
+ZONE_ID="Z02155239UQ5JEVY7T4S"
+DOMAIN_NAME="vamsimln.online"
 
 for instance in $@
 do
@@ -10,9 +12,31 @@ do
    
     if [ $instance != 'frontend' ]; then
         IP=$(aws ec2 describe-instances --instance-ids $InstanceID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+
     else
         IP=$(aws ec2 describe-instances --instance-ids $InstanceID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 
     echo "$instance: $IP"
+
+     aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Upsert"
+            ,"Changes": [{
+            "Action"              : "CREATE"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$RECORD_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                    "Value"         : "'" $IP "'"
+                }]
+            }
+            }]
+        }
+        '
 done 
