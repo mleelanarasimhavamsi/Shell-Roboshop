e85add5106a7f6013a109b87a38c618e38bd602b
vamsi <mleelanarasimhavamsi@gmail.com>
2025-12-27 21:17:31 +0530

    addingnew work

---
+++ Catalouge.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MONGODB_HOST=mongodb.vamsimln.online
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e " $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable Node"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable Node"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Instaling node"
+
+
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Creating System user"
+
+mkdir /app 
+VALIDATE $? "Creating app Directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Catalouge Application"
+
+cd /app 
+VALIDATE $? "Changing App Directory"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzip Catalouge"
+
+npm install 
+VALIDATE $? "Install Dependecies"
+
+cp Catalouge.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copy systemctl service"
+
+systemctl daemon-reload
+VALIDATE $? ""
+
+systemctl enable catalogue 
+VALIDATE $? "Enable Catalouge"
+
+systemctl start catalogue
+
+
+cp Catalouge.service /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy Mongo repo"
+
+[mongodb-org-7.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
+enabled=1
+gpgcheck=0
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "Install MongoDb Client"
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js
+VALIDATE $? "Load Catalouge products"
+
+mongosh --host MONGODB-SERVER-IPADDRESS
